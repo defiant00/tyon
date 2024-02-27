98a9be9ac228bb34b7ce8b73d616732c8815d792
defiant00 <defiant00@gmail.com>
2024-02-27 14:02:15 -0800

    version and help

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,39 @@
 const std = @import("std");
 
+const tyon_version = std.SemanticVersion{ .major = 0, .minor = 5, .patch = 0 };
+const spec_version = std.SemanticVersion{ .major = 1, .minor = 0, .patch = 0, .pre = "rc.1" };
+const data_version = std.SemanticVersion{ .major = 1, .minor = 0, .patch = 0, .pre = "rc.1" };
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const args = try std.process.argsAlloc(alloc);
+    defer std.process.argsFree(alloc, args);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    if (args.len == 2 and std.ascii.eqlIgnoreCase(args[1], "help")) {
+        printUsage();
+    } else if (args.len == 2 and std.ascii.eqlIgnoreCase(args[1], "version")) {
+        std.debug.print("tyon {}\nspec {}\ndata {}\n", .{ tyon_version, spec_version, data_version });
+    } else {
+        printUsage();
+        return error.InvalidCommand;
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn printUsage() void {
+    std.debug.print(
+        \\Usage: tyon [command]
+        \\
+        \\Commands:
+        \\  format [files]      Format specified files
+        \\  to-json [files]     Save files as JSON
+        \\  validate [files]    Validate specified files
+        \\
+        \\  debug [files]       Debug specified files
+        \\
+        \\  help                Print this help and exit
+        \\  version             Print version and exit
+        \\
+    , .{});
 }
